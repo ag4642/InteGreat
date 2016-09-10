81c50c622bb4e4651d085cc6669f472415f4a831
Shravan <sanjivshravan@gmail.com>
2016-09-10 05:42:10 -0400

    flask prototype

--- hitex_flask/hitex_backend.py
+++ hitex_flask/hitex_backend.py
@@ -10,17 +10,22 @@ db = mongo['we<3mongo']
 def add_to_workbook():
     if request.method == 'POST':
         jpeg = request.form['jpeg']
-        book = request.form['name']
+        name = request.form['name']
 
-        if db.books.count() == 0:
-            db.books.createCollection(book)
+        if db.books.count() == 0 or db.books.name.count() == 0:
+            db.books.name.insert_one({
+                'name' : name,
+                'images' : [jpeg]
+            })
 
         else:
+            db.books.update_one(
+                {'name' : name},
+                {"$addToSet" : {"images" : [jpeg] } }
+            )
 
 
-
-
-@app.route('/get', methods=['GET'])
+@app.route('/get', methods = ['GET'] )
 def get_workbook():
     if request.method == 'GET':
         name = request.args.get['name']
